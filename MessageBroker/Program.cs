20e47b35a3d89e7ac59b7b23500e0da3eee957f4
JayCeD1 <jesseeren@gmail.com>
2023-11-30 12:57:28 +0300

    create topic

--- MessageBroker/Program.cs
+++ MessageBroker/Program.cs
@@ -1,4 +1,5 @@
 using MessageBroker.Data;
+using MessageBroker.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,60 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Create Topic
+app.MapPost("api/topics", async (AppDbContext context, Topic topic) =>
+{
+    await context.Topics.AddAsync(topic);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/topics/{topic.Id}", topic);
+});
+
+//Return all Topics
+app.MapGet("api/topics", async (AppDbContext context) =>
+{
+    var topics = await context.Topics.ToListAsync();
+
+    return Results.Ok(topics);
+});
+
+//Publish Message 
+app.MapPost("api/topics/{id:int}/messages", async (AppDbContext context, int id, Message message) =>
+{
+    bool topics = await context.Topics.AnyAsync(topic => topic.Id == id);
+
+    if (!topics)
+    {
+        return Results.NotFound("Topics not found");
+    }
+    
+    //If no subscriptions do this
+    var subs = context.Subscriptions.Where(sub => sub.TopicId == id);
+
+    if (!subs.Any())
+    {
+        return Results.NotFound("There are no subs to this topic");
+    }
+
+    foreach (var sub in subs)
+    {
+        var msg = new Message
+        {
+            TopicMessage = message.TopicMessage,
+            SubscriptionId = sub.Id,
+            ExpiresAfter = message.ExpiresAfter,
+            MessageStatus = message.MessageStatus
+        };
+
+        await context.Messages.AddAsync(msg);
+    }
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok("Message has been published");
+});
 
 app.UseAuthorization();
 
